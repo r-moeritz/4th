fac2ceb2e81b1116d523e4a6e759ef9da1259946
Ralph Moeritz <426109+rmoritz@users.noreply.github.com>
2024-04-26 11:18:25 +0930

    Fix build with modern gcc on Linux

--- compile_64.sh
+++ compile_64.sh
@@ -1,9 +1,8 @@
-
 rm -f *.o
-gcc -Wall -DOS_LINUX -DSYSTEM_64 -c quattro_parser.c quattro_vm.c quattro_istruzioni.c utils.c quattro_debug.c quattro_istruzioni_aritmetiche.c quattro_istruzioni_flow.c quattro_istruzioni_mem.c quattro_istruzioni_stack.c quattro_istruzioni_definition.c quattro_istruzioni_ffi.c quattro_istruzioni_wordlist.c
+gcc -Wall -fpic -DOS_LINUX -DSYSTEM_64 -c quattro_parser.c quattro_vm.c quattro_istruzioni.c utils.c quattro_debug.c quattro_istruzioni_aritmetiche.c quattro_istruzioni_flow.c quattro_istruzioni_mem.c quattro_istruzioni_stack.c quattro_istruzioni_definition.c quattro_istruzioni_ffi.c quattro_istruzioni_wordlist.c quattro_istruzioni_repl.c
 
-rm -f lib4th.a
-ar r lib4th.a *.o
+rm -f lib4th.so
+gcc -shared -o lib4th.so *.o
 
-gcc -o forthconsole -Wall -DOS_LINUX -g forthconsole.c -L. -l4th -ldl
-gcc -o forthc -Wall -DOS_LINUX -g forthc.c -L. -l4th -ldl
+gcc -o forthconsole -Wall -DOS_LINUX -g forthconsole.c -L. -Wl,-rpath=. -l4th -ldl
+gcc -o forthc -Wall -DOS_LINUX -g forthc.c -L. -Wl,-rpath=. -l4th -ldl
